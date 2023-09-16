f78e35af9d316c63f570bbadbdf608a5068e6aa9
David Stinnett <davidstinnett@icloud.com>
2023-09-15 23:22:51 -0400

    complete

--- README.md
+++ README.md
@@ -6,42 +6,71 @@ This challenge allows you to practice the concepts and techniques learned over t
 
 This is an individual assessment. All work must be your own. Your challenge score is a measure of your ability to work independently using the material covered through this sprint. You need to demonstrate proficiency in the concepts and objectives introduced and practiced in preceding days.
 
-You are not allowed to collaborate during the sprint challenge. 
+You are not allowed to collaborate during the sprint challenge.
 
 ## Introduction
 
-The index.js file contains all of your challenges. Please review it in full before answering the questions. If you complete the stretch goals please leave them in your file but commented out so that they do not affect the MVP tasks. 
+The index.js file contains all of your challenges. Please review it in full before answering the questions. If you complete the stretch goals please leave them in your file but commented out so that they do not affect the MVP tasks.
 
-In meeting the minimum viable product (MVP) specifications listed below, you should have all tests passing. You can console.log to check your work and ensure you are submitting the correct results. 
+In meeting the minimum viable product (MVP) specifications listed below, you should have all tests passing. You can console.log to check your work and ensure you are submitting the correct results.
 
 ### Commits
 
-Set up codegrade early and commit your code regularly and meaningfully. 
+Set up codegrade early and commit your code regularly and meaningfully.
 
 ## Interview Questions
+
 ### (please edit this file and write your answer below each question.)
+
 Demonstrate your understanding of this week's concepts by answering the following free-form questions.
 
 Edit this document to include your answers after each question. Make sure to leave a blank line above and below your answer so it is clear and easy to read.
 
-1. Explain the differences between `.map`, `.reduce` and `.filter` and describe a use case for each. 
+1. Explain the differences between `.map`, `.reduce` and `.filter` and describe a use case for each.
+   .map, .reduce, and .filter are all array methods in JavaScript that help you work with arrays in a more efficient way.
+
+   .map: This method creates a new array by applying a function to every element in an existing array. It is useful when you want to create a new array where each element is derived from the elements of another array. For example, you can use .map to create a new array of the squares of the numbers in an existing array of numbers.
+
+   .reduce: This method reduces an array to a single value by applying a function that you provide to each element in the array, from left to right. The function takes two arguments: an accumulator and the current value. It is commonly used to find the sum of all the numbers in an array or to flatten nested arrays.
+
+   .filter: This method creates a new array including elements where the filter function returns true and omitting the ones where it returns false. A common use case is to create a new array that contains only certain elements from an existing array, for example, filtering an array of numbers to only include numbers greater than a certain value.
 
 2. Explain the difference between a callback and a higher order function.
+   A callback function is a function that is passed as an argument to another function and is invoked inside that function. A higher-order function is a function that accepts another function as an argument and/or returns a function as a result.
+
+   In essence, a higher-order function is a function that works with callbacks, either by taking a function as an argument or by returning a function. A very common use of higher-order functions in JavaScript is event handlers and listeners, where a function is passed to handle events.
 
 3. Explain what a closure is.
 
+A closure is a feature in JavaScript where an inner function has access to the outer (enclosing) function’s variables — a scope chain. The closure has three scope chains: it has access to its own scope (variables defined between its curly brackets), it has access to the outer function’s variables, and it has access to the global variables.
+
+This makes it possible to use closures to create a sort of private variables, helping to avoid variable name collisions and providing encapsulation.
+
 4. Describe the four principles of the 'this' keyword.
+
+The four principles of the 'this' keyword in JavaScript are:
+
+Global/Window Binding: When this is used in the global scope, it refers to the window object in the browser or the global object in Node.js.
+
+Implicit Binding: When a function is called with an object to its left, this refers to the object to the left of the dot at the call time. This is the most common use case in object methods.
+
+New Binding: When a function is called with the new keyword as a constructor, this refers to the new object that is created.
+
+Explicit Binding: You can explicitly set what this refers to using call, apply, or bind methods.
 
 5. Why do we need super() in an extended class?
 
-You are expected to be able to answer questions in these areas. Your responses contribute to your Sprint Challenge grade. 
+We need super() in an extended class to call the constructor of the superclass. This allows you to access and call functions on an object's parent class with JavaScript’s built-in super keyword.
+
+Using super() in the constructor of the subclass makes sure that the constructor of the superclass is called first and that the properties of the superclass are properly initialized before any work in the subclass’s constructor is done. It's required to properly set up the prototype chain, ensuring that the inheritance is properly established.
+
+You are expected to be able to answer questions in these areas. Your responses contribute to your Sprint Challenge grade.
 
 ## Instructions
 
 ### Task 1: Set up Project
 
 Using VSCode and Command Line:
-
 
 1. Fork the repo
 2. Go into canvas and connect your repo to codegrade
@@ -52,13 +81,12 @@ Using VSCode and Command Line:
 7. next type `npm run test` in your terminal
 8. Complete your work making regular commits to main/master; your codegrade score will update each time you make a push.
 
-
 ### Testing & Debugging
 
 Open a second terminal inside of your project by clicking on the split terminal icon
 ![alt text](assets/split_terminal.png "Split Terminal")
 
-Inside of your second terminal type `npm start` 
+Inside of your second terminal type `npm start`
 ![alt text](assets/npm_start.png "type npm start")
 
 You will be running your tests in one terminal and debugging in the other. As you work on your code you should make use of `console.log` to check your progress and debug.
@@ -71,8 +99,8 @@ You must complete all tasks inside of `index.js` and answer the questions above.
 In your solutions, it is essential that you follow best practices and produce clean and professional results. Schedule time to review, refine, and assess your work and perform basic professional polishing including spell-checking and grammar-checking on your work. It is better to submit a challenge that meets MVP than one that attempts too much and does not.
 
 ## Resources
- 
- [Sprint Challenge Study Guide](https://www.notion.so/bloomtech/Unit-1-Sprint-3-Study-Guide-033a9a00659a4ef98c12eb97e49a6110)
+
+[Sprint Challenge Study Guide](https://www.notion.so/bloomtech/Unit-1-Sprint-3-Study-Guide-033a9a00659a4ef98c12eb97e49a6110)
 
 ## Submission format
 
--- index.js
+++ index.js
@@ -15,11 +15,10 @@ function myFunction() {
 }
 //myFunction();
 
-//🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
+//🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀:
 
-
-
-
+//In JavaScript, functions create a scope, meaning variables defined within a function cannot be accessed from outside that function. However, nested functions have access to variables defined in their outer function. This is due to a concept in JavaScript called
+// "closure". nestedFunction is able to access the internal variable because nestedFunction is closed over the scope of myFunction, hence creating a closure. This closure allows nestedFunction to retain access to all the variables in its containing function’s scope.
 
 /* 🚀🚀🚀 Task 2: Counter 🚀🚀🚀 */
 /* Use summation to do the following:
@@ -30,29 +29,82 @@ function myFunction() {
 💡 NOTE: you may use a for loop for this function if you wish 
 */
 
-function summation(/*Your Code Here*/) {
+function summation(num) {
   /*Your Code Here*/
-
+  let sum = 0;
+  for (let i = 1; i <= num; i++) {
+    sum += i;
   }
- 
+  return sum;
+}
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
 // Given this zoo data from around the United States, follow the instructions below. Use the specific array methods in the requests below to solve the problems.
 
 const zooAnimals = [
-    { animal_name: "Jackal, asiatic", population: 5, scientific_name: "Canis aureus", state: "Kentucky" },
-    { animal_name: "Screamer, southern", population: 1, scientific_name: "Chauna torquata", state: "Alabama" },
-    { animal_name: "White spoonbill", population: 8, scientific_name: "Platalea leucordia", state: "Georgia" },
-    { animal_name: "White-cheeked pintail", population: 1, scientific_name: "Anas bahamensis", state: "Oregon" },
-    { animal_name: "Black-backed jackal", population: 2, scientific_name: "Canis mesomelas", state: "Washington" },
-    { animal_name: "Brolga crane", population: 9, scientific_name: "Grus rubicundus", state: "New Mexico" },
-    { animal_name: "Common melba finch", population: 5, scientific_name: "Pytilia melba", state: "Pennsylvania" },
-    { animal_name: "Pampa gray fox", population: 10, scientific_name: "Pseudalopex gymnocercus", state: "Connecticut" },
-    { animal_name: "Hawk-eagle, crowned", population: 10, scientific_name: "Spizaetus coronatus", state: "Florida" },
-    { animal_name: "Australian pelican", population: 5, scientific_name: "Pelecanus conspicillatus", state: "West Virginia" },
-  ];
-  
-  /* 🦁🦁🦁 Request 1: .forEach() 🦁🦁🦁
+  {
+    animal_name: "Jackal, asiatic",
+    population: 5,
+    scientific_name: "Canis aureus",
+    state: "Kentucky",
+  },
+  {
+    animal_name: "Screamer, southern",
+    population: 1,
+    scientific_name: "Chauna torquata",
+    state: "Alabama",
+  },
+  {
+    animal_name: "White spoonbill",
+    population: 8,
+    scientific_name: "Platalea leucordia",
+    state: "Georgia",
+  },
+  {
+    animal_name: "White-cheeked pintail",
+    population: 1,
+    scientific_name: "Anas bahamensis",
+    state: "Oregon",
+  },
+  {
+    animal_name: "Black-backed jackal",
+    population: 2,
+    scientific_name: "Canis mesomelas",
+    state: "Washington",
+  },
+  {
+    animal_name: "Brolga crane",
+    population: 9,
+    scientific_name: "Grus rubicundus",
+    state: "New Mexico",
+  },
+  {
+    animal_name: "Common melba finch",
+    population: 5,
+    scientific_name: "Pytilia melba",
+    state: "Pennsylvania",
+  },
+  {
+    animal_name: "Pampa gray fox",
+    population: 10,
+    scientific_name: "Pseudalopex gymnocercus",
+    state: "Connecticut",
+  },
+  {
+    animal_name: "Hawk-eagle, crowned",
+    population: 10,
+    scientific_name: "Spizaetus coronatus",
+    state: "Florida",
+  },
+  {
+    animal_name: "Australian pelican",
+    population: 5,
+    scientific_name: "Pelecanus conspicillatus",
+    state: "West Virginia",
+  },
+];
+
+/* 🦁🦁🦁 Request 1: .forEach() 🦁🦁🦁
   The zoos want to display both the scientific name and the animal name in front of the habitats. 
     1. Receive the zooAnimals array as an argument passed from a parameter 
     2. Use .forEach() to populate a new array called displayNames that will be an array of strings with only the animal name and scientific name of each animal
@@ -60,12 +112,20 @@ const zooAnimals = [
   💡 NOTE: the array returned should be an array of strings, and each string should follow this pattern: "name: {name}, scientific: {scientific name}"
   */
 
-  function animalNames(/*Your Code Here*/){
-    /*Your Code Here*/
-  }
-  
+function animalNames(zooAnimals) {
+  /*Your Code Here*/
+  let displayNames = [];
 
-  /* 🦁🦁🦁 Request 2: .map() 🦁🦁🦁
+  zooAnimals.forEach((animal) => {
+    displayNames.push(
+      `name: ${animal.animal_name}, scientific: ${animal.scientific_name}`
+    );
+  });
+
+  return displayNames;
+}
+
+/* 🦁🦁🦁 Request 2: .map() 🦁🦁🦁
   The zoo needs a list of all their animal's names converted to lower case. 
   Use lowerCaseNames to do the following:
   1. Receive the zooAnimals array as an argument passed from a parameter 
@@ -75,25 +135,24 @@ const zooAnimals = [
   💡 NOTE: Do some research for other methods that can help help you
   */
 
-  function lowerCaseNames(/*Your Code Here*/){
-    /*Your Code Here*/
-  }
-  
-  
-  /* 🦁🦁🦁 Request 3: .filter() 🦁🦁🦁
+function lowerCaseNames(zooAnimals) {
+  /*Your Code Here*/
+  return zooAnimals.map((animal) => animal.animal_name.toLowerCase());
+}
+
+/* 🦁🦁🦁 Request 3: .filter() 🦁🦁🦁
   The zoo is concerned about animals with a lower population count. 
   Use lowPopulationAnimals to do the following: 
   1. Receive the zooAnimals array as an argument passed from a parameter 
   2. Use .filter() to create a new array of objects which contains only the animals with a population of less than 5
   3. Return this new array
   */
+function lowPopulationAnimals(zooAnimals) {
+  /*Your Code Here*/
+  return zooAnimals.filter((animal) => animal.population < 5);
+}
 
-  function lowPopulationAnimals(/*Your Code Here*/){
-    /*Your Code Here*/
-  }
-  
-
-  /* 🦁🦁🦁 Request 4: .reduce() 🦁🦁🦁
+/* 🦁🦁🦁 Request 4: .reduce() 🦁🦁🦁
   The zoo needs to know their total animal population across the United States. 
   USe USApop to do the following:
   1. Receive the zooAnimals array as an argument passed from a parameter 
@@ -102,13 +161,13 @@ const zooAnimals = [
   💡 NOTE: Remember the reduce method takes two arguments: a callback (which itself takes two args - the accumulator and the item), and an initial value for the count. Check MDN/W3Schools for syntax!
   */
 
-  function USApop(/*Your Code Here*/){
-    /*Your Code Here*/
-  }
-  
-  
-  // 🦁🦁🦁 Callbacks 🦁🦁🦁  
-  /* 🦁🦁🦁 Step 1: Create a higher-order function 🦁🦁🦁
+function USApop(zooAnimals) {
+  /*Your Code Here*/
+  return zooAnimals.reduce((acc, animal) => acc + animal.population, 0);
+}
+
+// 🦁🦁🦁 Callbacks 🦁🦁🦁
+/* 🦁🦁🦁 Step 1: Create a higher-order function 🦁🦁🦁
   Use the higher-order function called consume to do the following:
     1. Receive 3 parameters: a, b and cb. The first two parameters (a and b) can take any argument (we can pass any value as an argument) and the last parameter (cb) accepts a callback
     2. Return the invocation of cb taking `a` and `b` as its arguments
@@ -116,51 +175,48 @@ const zooAnimals = [
     💡 NOTE: The tests for 'consume' will pass if it is created correctly and also after you correctly complete the functions 'add' and 'greeting' below in Step 2.
   */
 
-  function consume(/*Your Code Here */){
-    /*Your Code Here */
-  }
- 
-  
-  // 🦁🦁🦁 Step 2: Create several functions to callback with consume(); 🦁🦁🦁
+function consume(a, b, cb) {
+  /*Your Code Here */
+  return cb(a, b);
+}
 
- /*  Use add to do the following:
+// 🦁🦁🦁 Step 2: Create several functions to callback with consume(); 🦁🦁🦁
+
+/*  Use add to do the following:
  1. Receive two numbers as an argument that are passed in from its first and second parameters
  2. Return the sum of those numbers
  */
 
-function add(/*Your Code Here */){
-    /*Your Code Here*/
-  }
-
+function add(a, b) {
+  /*Your Code Here*/
+  return a + b;
+}
 
 /* Use multiply to do the following:
 1. Receive two numbers as an argument that are passed in from its first and second parameters
 2. Return the product of those numbers
 */
 
-function multiply(/*Your Code Here */){
-   /*Your Code Here */
-  }
+function multiply(a, b) {
+  /*Your Code Here */
+  return a * b;
+}
 
-
- /* Use greeting to do the following:
+/* Use greeting to do the following:
 1. Receive two strings (a first name and last name) as an argument that are passed in from its first and second parameters
 2. Return "Hello {first-name} {last-name}, nice to meet you!"
 💡 NOTE: The string returned must match the format above or the test will not pass!
 */
 
-function greeting(/*Your Code Here */){
-   return /*Your Code Here */
-  }
-  
-  
-// 🦁🦁🦁 Step 3: Check your work by un-commenting the following calls to consume(): 🦁🦁🦁 
+function greeting(firstName, lastName) {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`; /*Your Code Here */
+}
+
+// 🦁🦁🦁 Step 3: Check your work by un-commenting the following calls to consume(): 🦁🦁🦁
 // ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
-
-
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 // 🐴🐴🐴 Topic 3: Prototypes 🐴🐴🐴 //
 
@@ -175,79 +231,92 @@ function greeting(/*Your Code Here */){
 - Instances of CuboidMaker should initialize `length`, `width` and `height` properties
 */
 
-function CuboidMaker(/*Your Code Here */){
+function CuboidMaker(attributes) {
   /*Your Code Here */
+  this.length = attributes.length;
+  this.width = attributes.width;
+  this.height = attributes.height;
 }
-
 
 /* 🐴🐴🐴 Step 2: Volume Method 🐴🐴🐴
   Create a method called volume using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
   💡 NOTE: Formula for cuboid volume: length * width * height   
 */
-
-
-
+CuboidMaker.prototype.volume = function () {
+  return this.length * this.width * this.height;
+};
 
 /* 🐴🐴🐴 Step 3: Surface Area Method 🐴🐴🐴
   Create another method called surfaceArea using CuboidMaker's prototype that returns the surface area of a given cuboid's length, width, and height. 
   💡 NOTE: Formula for cuboid surface area: 2 * (length * width + length * height + width * height)  
 */
-
-
-
+CuboidMaker.prototype.surfaceArea = function () {
+  return (
+    2 *
+    (this.length * this.width +
+      this.length * this.height +
+      this.width * this.height)
+  );
+};
 
 /* 🐴🐴🐴 Step 4: Create a new object that uses CuboidMaker (not auto graded)🐴🐴🐴
   Create an object called cuboid that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid. */
 
-
-
-
+const cuboid = new CuboidMaker({ length: 4, width: 5, height: 5 });
 
 // 🐴🐴🐴 Test your volume and surfaceArea methods by uncommenting the logs below: 🐴🐴🐴
 // ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️ ⬇️
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
- 
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
 // 🦄🦄🦄 Topic 4: Classes 🦄🦄🦄 //
 //Using CuboidMakerTwo, take your prototypes from above and refactor into class syntax. Then, create an object called cuboidTwo that uses the new keyword to use our CuboidMakerTwo class.
- 
-class CuboidMakerTwo{
 
+class CuboidMakerTwo {
+  constructor(attributes) {
+    this.length = attributes.length;
+    this.width = attributes.width;
+    this.height = attributes.height;
+  }
+
+  volume() {
+    return this.length * this.width * this.height;
+  }
+
+  surfaceArea() {
+    return (
+      2 *
+      (this.length * this.width +
+        this.length * this.height +
+        this.width * this.height)
+    );
+  }
 }
 
-
-
+const cuboidTwo = new CuboidMakerTwo({ length: 4, width: 5, height: 5 });
 
 //🦄🦄🦄 Test your volume and surfaceArea methods by uncommenting the logs below: 🦄🦄🦄
-// console.log(cuboidTwo.volume()); // 100
-// console.log(cuboidTwo.surfaceArea()); // 130
+console.log(cuboidTwo.volume()); // 100
+console.log(cuboidTwo.surfaceArea()); // 130
 
-
-
-
-
-  
-
-
-  /* 🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑 */
-  function foo(){
-    console.log('its working');
-    return 'bar';
-  }
-  foo();
-  module.exports = {
-    foo,
-    summation,
-    animalNames,
-    lowerCaseNames,
-    lowPopulationAnimals,
-    USApop,
-    consume, 
-    add,
-    multiply,
-    greeting,
-    CuboidMaker,
-    CuboidMakerTwo
-  }
+/* 🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑 */
+function foo() {
+  console.log("its working");
+  return "bar";
+}
+foo();
+module.exports = {
+  foo,
+  summation,
+  animalNames,
+  lowerCaseNames,
+  lowPopulationAnimals,
+  USApop,
+  consume,
+  add,
+  multiply,
+  greeting,
+  CuboidMaker,
+  CuboidMakerTwo,
+};
